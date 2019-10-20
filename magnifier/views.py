4244a6cfbc97920ca2cc1593eb9719bb9be78e62
callmeBoris <boriskayienzo@gmail.com>
2019-10-20 20:54:59 +0200

    added images results

--- magnifier/views.py
+++ magnifier/views.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from .models import Search
 
 
-BASE_YAHOO_URL = 'https://search.yahoo.com/search?p={}'
+BASE_WEBDESIGNERDEPOT_URL = 'https://www.webdesignerdepot.com/?s={}'
 
 # Create your views here.
 def home(request):
@@ -17,17 +17,23 @@ def results(request):
     search = request.POST.get('search')
     Search.objects.create(search_text=search)
 
-    final_url = BASE_YAHOO_URL.format(quote_plus(search))
+    final_url = BASE_WEBDESIGNERDEPOT_URL.format(quote_plus(search))
     
     response = requests.get(final_url)
     data = response.text
     soup = BeautifulSoup(data, features='html.parser')
 
-    result_list = soup.find_all('div', {'class': 'algo-sr'})
-    result_tilte = result_list[0].find(class_='compTitle').text
-    result_url = result_list[0].find('a').get('href')
+    result_list = soup.find_all('article', {'class': 'list-post'})
     
-    print(result_tilte)
-    print(result_url)
+    results = []
 
-    return render(request, 'magnifier/results.html', {'search': search})
+    for result in result_list:
+        result_category = result.find(class_='hp-category').text
+        result_tilte = result.find(class_='main-post-title').text
+        result_excerpt = result.find(class_='hp-excerpt').text
+        result_url = result.find('a').get('href')
+        result_image = result.find('img').get('src')
+
+        results.append((result_category, result_tilte, result_url, result_excerpt, result_image))
+
+    return render(request, 'magnifier/results.html', {'search': search, 'results': results})
