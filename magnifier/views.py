5ea3c1b5fcdaba7f1f97c3ce9a626f4209706a28
callmeBoris <boriskayienzo@gmail.com>
2019-10-20 03:41:55 +0200

    connected to yahoo search engine

--- magnifier/views.py
+++ magnifier/views.py
@@ -1,5 +1,11 @@
+import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from .models import Search
+
+
+BASE_YAHOO_URL = 'https://search.yahoo.com/search?p={}'
 
 # Create your views here.
 def home(request):
@@ -7,5 +13,21 @@ def home(request):
 
 
 def results(request):
+
     search = request.POST.get('search')
+    Search.objects.create(search_text=search)
+
+    final_url = BASE_YAHOO_URL.format(quote_plus(search))
+    
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    result_list = soup.find_all('div', {'class': 'algo-sr'})
+    result_tilte = result_list[0].find(class_='compTitle').text
+    result_url = result_list[0].find('a').get('href')
+    
+    print(result_tilte)
+    print(result_url)
+
     return render(request, 'magnifier/results.html', {'search': search})
